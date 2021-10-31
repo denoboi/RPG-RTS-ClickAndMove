90a5b988b0aa53edb6fa6c1224b59a0a67935e5f
denoboi <deniz.kaymakci@hotmail.com>
2021-10-31 10:03:02 +0300

    Movement

---
+++ Assets/[Game]/Scripts/Mover.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    NavMeshAgent navMeshAgent;
+
+    Ray ray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        //navMeshAgent.destination = target.position;
+
+        if (Input.GetMouseButton(1))
+        {
+            MoveToCursor();
+        }
+            
+    }
+
+    void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        Debug.DrawRay(ray.origin, ray.direction * 299);
+
+        if (hasHit)
+        {
+            navMeshAgent.destination = hit.point;
+        }
+    }
+}
